2ed7f9da3ecf5cf3c2f4e9d55445aca83fcd94bd
20rwplay@gmail.com <20rwplay@gmail.com>
2024-05-19 16:12:43 +0500

    Выполненная работа на оценку хорошо. Паксюаткин

--- PR33_Паксюаткин/MainWindow.xaml.cs
+++ PR33_Паксюаткин/MainWindow.xaml.cs
@@ -1,30 +1,23 @@
-﻿using System;
+﻿using PR33_Паксюаткин.Pages;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PR33_Паксюаткин
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+
+            init = this;
+            LoadData();
+            OpenPage(new Groups());
         }
 
         public class GroupsList
--- PR33_Паксюаткин/Pages/Groups.xaml.cs
+++ PR33_Паксюаткин/Pages/Groups.xaml.cs
@@ -1,41 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using static PR33_Паксюаткин.MainWindow;
 
 namespace PR33_Паксюаткин.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для Groups.xaml
-    /// </summary>
     public partial class Groups : Page
     {
         public Groups()
         {
             InitializeComponent();
+            LoadData();
         }
-
         private void FindGroups(object sender, TextChangedEventArgs e)
         {
-
+            string searchText = find.Text.ToLower();
+            listView.Items.Clear();
+            foreach (GroupsList group in init.groupsLists)
+            {
+                if (group.name.ToLower().Contains(searchText))
+                {
+                    listView.Items.Add(group);
+                }
+            }
         }
-
         public void LoadData()
         {
             listView.Items.Clear();
-            for (int i = 0; i < mainWindow.groupsLists.Count; i++)
+            for (int i = 0; i < init.groupsLists.Count; i++)
             {
-                listView.Items.Add(mainWindow.groupsLists[i]);
+                listView.Items.Add(init.groupsLists[i]);
             }
         }
     }
